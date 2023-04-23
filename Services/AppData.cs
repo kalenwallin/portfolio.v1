b862f1b83395a7d80fdc233971918e55066e3017
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-04-23 01:56:24 -0500

    Update image links

--- Services/AppData.cs
+++ Services/AppData.cs
@@ -8,6 +8,7 @@ namespace Blazor.Services
     public class AppData
     {
         public string Theme { get; set; }
+        public string imgLink = "https://raw.githubusercontent.com/kalenwallin/portfolio.v1/gpages/img/";
         public event Action OnChange;
 
         public void SetTheme(string theme)
@@ -19,6 +20,10 @@ namespace Blazor.Services
         private void NotifyStateChanged()
         {
             OnChange?.Invoke();
+        }
+        public string GetImageLink(string image)
+        {
+            return $"{imgLink}{image}";
         }
     }
 }
