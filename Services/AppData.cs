fa8b966f6706aee2a87b9b311833465c4d99c186
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-05-10 15:19:11 -0500

    Fixed typo in title

--- Services/AppData.cs
+++ Services/AppData.cs
@@ -10,7 +10,7 @@ namespace Blazor.Services
         public string Theme { get; set; }
         public string imgLink = "https://raw.githubusercontent.com/kalenwallin/portfolio.v1/gpages/img/";
         public string description = "Showcasing his skills through jobs, projects, school transcripts, community service, and life experiences.";
-        public string title = "Kalen Walliin v1";
+        public string title = "Kalen Wallin v1";
         public event Action OnChange;
 
         public void SetTheme(string theme)
